7679aaafa0f5d7ccdebe7450e03cc23d8c7aba77
Elena Miagkaia <miagkaia.e@gmail.com>
2025-04-04 12:43:32 +0300

    3,4,5 задачи

--- навигация по DOM/script.js
+++ навигация по DOM/script.js
@@ -33,3 +33,61 @@ for (let p of allP) {
 		console.log(p.textContent);
 	}
 }
+
+// задача 3
+const pHighlight = document.querySelector('p.highlight');
+console.log(pHighlight);
+// выведет элемент, если родитель соотвествует
+console.log(pHighlight.parentElement.closest('div#root'));
+// вывод true или false
+console.log(!!pHighlight.parentElement.closest('div#root'));
+
+
+// второй вариант
+const pHighlightParent = pHighlight.closest('article');
+console.log(pHighlightParent);
+console.log(!!pHighlightParent.closest('div#root'))
+
+// задача 4
+const h1Title = document.querySelector('h1.title');
+console.log(h1Title);
+const h1TitleParent = h1Title.parentElement
+console.log(h1TitleParent.childNodes.length);
+if (h1Title.nextElementSibling) {
+	console.log(h1Title.nextElementSibling.textContent);
+};
+
+// второй вариант
+const parentTitle = h1Title.closest('header');
+console.log(parentTitle.childNodes.length);
+if (h1Title.nextElementSibling) {
+	console.log(h1Title.nextElementSibling.textContent);
+}
+
+// задача 5
+// первый вариант
+const users = document.getElementById('users');
+console.log(users)
+for (let tr of users.rows) {
+	if (tr.classList.contains('user-row')) {
+		console.log(tr.cells[0].textContent)
+	}
+};
+console.log(users.rows[0].cells[1].matches('.age'));
+
+// второй вариант
+for (let tr of users.rows) {
+	if (tr.matches('.user-row')) {
+		console.log(tr.cells[0].textContent)
+	}
+};
+const usersTh = users.rows[0];
+console.log(usersTh);
+console.log(usersTh.cells[1].matches('.age'))
+
+// третий
+const userRow = document.querySelector('tr.user-row') 
+if (users.contains(userRow)) {
+	console.log(userRow.cells[0].textContent)
+}
+console.log(users.rows[0].cells[1].matches('.age'));
